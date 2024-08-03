d55b09164f87396ba5c387a1762b3ccb4199e7f9
Daniel H <daniel.neves0021@gmail.com>
2024-08-03 18:42:41 -0300

    style: Update documented

--- Shared/Update.php
+++ Shared/Update.php
@@ -1,8 +1,18 @@
 <?php 
+// This file contains the update logic;
+// Include the connection logic;
 include("ConnectionMysql.php");
+
+// Get the user id from the URL;
 $id = $_GET['id'];
+
+// Get the data from ther form post;
 $newInfo = $_POST['newInfo'];
+
+// Execute the update;
 $mysqli->query("UPDATE Informations SET information='$newInfo' WHERE id=$id;");
+
+// Close the connection and redirect;
 $mysqli->close();
 header('Location: ../Views/Read.php');
 ?>
--- Views/Update.php
+++ Views/Update.php
@@ -7,42 +7,38 @@ $id = $_GET["id"];
 // Query Parameters;
 $table = "Informations";
 
-// Execute
+// Execute one query for get the database data from the id;
 $result = $mysqli->query("SELECT * FROM ".$table." WHERE Id= ".$id.";");
 $information = $result->fetch_array();
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-<?php include("../Shared/InitPage.php"); ?>
-</head>
-
+	<head>
+	<?php include("../Shared/InitPage.php"); ?>
+	</head>
 <body>
-<nav class="text-center mt-5">
-<a href="../Views/Create.php" class="mx-2">Data Creater</a>
-<a href="../Views/Read.php" class="mx-2">Data Manager</a>
-</nav>
+	<nav class="text-center mt-5">
+		<a href="../Views/Create.php" class="mx-2">Data Creater</a>
+		<a href="../Views/Read.php" class="mx-2">Data Manager</a>
+	</nav>
 <main>
 <div class="card mx-5 my-2">
 <div class="card-body">
-<form action=<?php echo "../Shared/Update.php?id=".$information['id'].""?> method="post">
-
-  <div class="mb-3">
-     <p>Informação antiga: <span style="margin-left:4.6rem;color:red" ><?php echo $information['information']; ?></span><p>
-  </div>
-<div class=container">
-  <div class="form-group row">
-<div class="col col-sm-2 mt-1">
-    <label for="newInfo" class="form-label">Nova Informação:</label>
+	<form action=<?php echo "../Shared/Update.php?id=".$information['id'].""?> method="post">
+ 		<div class="mb-3">
+     		<p>Informação antiga: <span style="margin-left:4.6rem;color:red" ><?php echo $information['information']; ?></span><p>
+ 		</div>
+	<div class=container">
+  	<div class="form-group row">
+	<div class="col col-sm-2 mt-1">
+    		<label for="newInfo" class="form-label">Nova Informação:</label>
+	</div>
+	<div class="col">
+    		<input type="text" class="form-control" id="newInfo" name="newInfo" width="50">
+	</div>  
 </div>
-<div class="col">
-    <input type="text" class="form-control" id="newInfo" name="newInfo" width="50">
-</div>  
-</div>
-
 </form>
 </div>
 </main>
 </body>
-
 </html>
